5fe2483be572904611a85a3230377a67ed1b6aa9
Alumno <fdsgsd>
2022-07-06 18:07:42 +0200

    Commit sexto. Implementacion de un segundo seekbar porque el primero daba problemas. Personalizacion terminada y realizacion de pruebas de funcionamiento. Los numeros se mueven al son del thumb y se quedan estaticos cuando deja de estar en uso

--- app/src/main/java/com/example/botones/SeekBarActivity.kt
+++ app/src/main/java/com/example/botones/SeekBarActivity.kt
@@ -2,6 +2,7 @@ package com.example.botones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import com.example.botones.databinding.ActivitySeekBarBinding
 
 class SeekBarActivity : AppCompatActivity() {
@@ -10,6 +11,23 @@ class SeekBarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySeekBarBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.seekBar2.setOnSeekBarChangeListener(object:
+        SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar,
+                                           progress: Int,
+                                           fromUser: Boolean) {
+                binding.tvSeekBar.text = seek.progress.toString().plus("/100")
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar?) {
+                //write custom code for progress is starting
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar?) {
+                //write custom code for progress is stopped
+            }
+        })
 
     }
 }
